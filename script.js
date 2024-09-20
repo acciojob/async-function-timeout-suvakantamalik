f00f8a19879a9187e82513678432762ac267fba8
suvakantamalik <134865642+suvakantamalik@users.noreply.github.com>
2024-09-20 16:44:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const text = document.getElementById("text");
+const delay = document.getElementById("delay");
+const btn = document.getElementById("btn");
+const output = document.getElementById("output");
+
+btn.addEventListener("click", () => {
+	let delayValue = parseInt(delay.value);
+	setTimeout(myFunc, delayValue);
+});
+function myFunc(){
+	output.innerHTML = text.value;
+}
